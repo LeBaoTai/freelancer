892c1c4ca331cec0bc111283d9cf5477e3202421
FiaT <79523633+LeBaoTai@users.noreply.github.com>
2023-12-11 14:48:25 +0700

    Update HomeController.java

--- src/main/java/com/lbt/freelancer/HomeController.java
+++ src/main/java/com/lbt/freelancer/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
   @GetMapping()
   public String home() {
-    return "Welcome to Java Spring Boot App";
+    return "Java Spring Boot App";
   }
 }
